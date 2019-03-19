e4d540d7666f284bf2d1771d7ace79583f138db0
Hussin Salah <hussin.web@computershop.delta>
2019-03-20 00:26:05 +0200

    category with redux and API finished

--- App.js
+++ App.js
@@ -81,6 +81,7 @@ export default class App extends Component {
     return (
       <Provider store={store}>
           {this.state.rootPage}
+          {/* <HomeScreen /> */}
       </Provider>
 
       // <Signin />
--- src/screens/CategoryNavigatorScreens/Home.js
+++ src/screens/CategoryNavigatorScreens/Home.js
@@ -17,6 +17,10 @@ import {
   removeOrientationListener as rol
 } from 'react-native-responsive-screen';
 
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { fetchCategories } from '../../actions/categoryActions';
+
 import Header from '../../components/Header';
 import cat1 from '../../assets/images/cat1.png';
 import cat2 from '../../assets/images/cat2.png';
@@ -24,27 +28,32 @@ import cat3 from '../../assets/images/cat3.png';
 import main_background from '../../assets/images/bg1.png';
 import headImage from '../../assets/images/headImage.png';
 
-class MainScreen extends Component {
+class Home extends Component {
 
     constructor(){
         super();
         this.state = {
             data: [
-                { image: cat1, cat: 'عسل' },
-                { image: cat2, cat: 'خضروات' },
-                { image: cat3, cat: 'نباتات' },
-                { image: cat1, cat: 'عسل' },
-                { image: cat2, cat: 'خضروات' },
-                { image: cat3, cat: 'نباتات' },
+                { image: cat1, name: 'عسل' },
+                { image: cat2, name: 'خضروات' },
+                { image: cat3, name: 'نباتات' },
+                { image: cat1, name: 'عسل' },
+                { image: cat2, name: 'خضروات' },
+                { image: cat3, name: 'نباتات' },
             ]
         }
     }
 
-    
+    componentDidMount() {
 
-    
+        this.props.fetchCategories()
+
+    }
 
     render(){
+        const { categories, isFetching } = this.props.categories
+        alert(JSON.stringify(categories));
+        alert(isFetching);
         return(
             <ImageBackground
              source={main_background}
@@ -57,7 +66,8 @@ class MainScreen extends Component {
                     <Image source={headImage} style={{ width: wp('100%'), height: hp('42%')}} />
 
                     <FlatList 
-                        data={this.state.data}
+                        // data={this.state.data}
+                        data={categories}
                         renderItem={({item}) =>{
                             return(
 
@@ -66,7 +76,7 @@ class MainScreen extends Component {
                                       <Image source={item.image} style={{ width: wp('84%'), height:hp('22%'), }}/>
 
                                       <View style={styles.catView}>
-                                          <Text style={[styles.catTextStyle, !I18nManager.isRTL?{borderTopLeftRadius: wp('1%'), borderBottomLeftRadius: wp('1%') }:{borderTopRightRadius: wp('1%'), borderBottomRightRadius: wp('1%') }]}>{item.cat}</Text>
+                                          <Text style={[styles.catTextStyle, !I18nManager.isRTL?{borderTopLeftRadius: wp('1%'), borderBottomLeftRadius: wp('1%') }:{borderTopRightRadius: wp('1%'), borderBottomRightRadius: wp('1%') }]}>{item.name}</Text>
                                       </View>
                                     </TouchableOpacity>
                                 </View>
@@ -101,4 +111,17 @@ const styles = StyleSheet.create({
     }
 })
 
-export default MainScreen
+
+function mapStateToProps(state) {
+    return {
+        categories: state.category
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        ...bindActionCreators({ fetchCategories }, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
